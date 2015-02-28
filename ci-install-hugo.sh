8b999f47b9b172787084ea4496b85c2994766b83
Nathan Youngman <git@nathany.com>
2015-02-28 02:06:04 -0700

    use hugo 0.13

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -1,7 +1,7 @@
 set -x
 set -e
 if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ]; then
-  wget https://github.com/spf13/hugo/releases/download/v0.12/hugo_0.12_linux_amd64.tar.gz
-  tar xvzf hugo_0.12_linux_amd64.tar.gz
-  cp hugo_0.12_linux_amd64/hugo_0.12_linux_amd64 $CIRCLE_BUILD_DIR/bin/hugo
+  wget https://github.com/spf13/hugo/releases/download/v0.13/hugo_0.13_linux_amd64.tar.gz
+  tar xvzf hugo_0.13_linux_amd64.tar.gz
+  cp hugo_0.13_linux_amd64/hugo_0.13_linux_amd64 $CIRCLE_BUILD_DIR/bin/hugo
 fi
