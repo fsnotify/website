f2eae3d23b524c3f3c3b508b321f16f944a9a5fc
Nathan Youngman <git@nathany.com>
2018-07-15 09:44:54 -0600

    CircleCI 2.0, s3deploy, Hugo 0.44, License

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -1,12 +1,11 @@
-HUGO_VERSION=0.18.1
-HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+HUGO_DOWNLOAD=hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 
 set -x
 set -e
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
-  tar xvzf ${HUGO_DOWNLOAD} "hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64"
-  mv  "hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64" $CIRCLE_BUILD_DIR/bin/hugo
+if [ ! -e $HOME/bin/hugo ] || ! [[ `$HOME/bin/hugo version` =~ v${HUGO_VERSION} ]]; then
+  wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
+  tar xvzf ${HUGO_DOWNLOAD} hugo
+  mv hugo $HOME/bin/hugo
 fi
